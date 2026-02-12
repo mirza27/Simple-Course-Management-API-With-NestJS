7496db70e8e2e28dd10b14a2d84b626d04ab58aa
mirza27 <ramadhanimirza10@gmail.com>
2026-02-12 14:41:24 +0700

    feat(Category): add api get all categories

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { JwtGuard } from '../auth/guard/jwt.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/database/entities/user.entity';
+import { RolesGuard } from '../auth/guard/roles.guard';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.CUSTOMER)
+  @Get('list')
+  async getCategories() {
+    return await this.categoryService.GetAllCategories();
+    return 'List of categories';
+  }
+}
--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -3,9 +3,10 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { Category } from 'src/database/entities/category.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), AuthModule],
   providers: [CategoryService],
   controllers: [CategoryController],
   exports: [CategoryService],
